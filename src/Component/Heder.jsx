3db7772930dd9847114479a69ca59532af528b1c
RezaualHossenSunny <91420025+RezaualHossenSunny@users.noreply.github.com>
2024-04-06 06:38:48 +0600

    baner done

--- src/Component/Heder.jsx
+++ src/Component/Heder.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/avada-taxi-logo.png'
 const Heder = () => {
   return (
 <>
-<div className='bg-heder bg-no-repeat bg-center bg-cover'>
+<div className='bg-heder bg-no-repeat bg-center bg-cover h-screen'>
         <div className='mx-auto max-w-container flex py-6'>
             <div className='w-1/5'>
                 <img src={logo}/>
@@ -20,6 +20,12 @@ const Heder = () => {
             <div className='w-[200px] h-[50px] py-4 mt-4 ml-16 rounded-md border-[#EFB317] bg-[#EFB317]'>
         <p className=' font-popins font-bold text-sm  text-center h-[30px] text-black'>BOOK ONLINE</p>
             </div>
+        </div>
+
+        <div className='mx-auto max-w-container py-40 '>
+        <h1 className='font-popins text-center text-[#EFB317] font-semibold text-[65px]'>Premium <span className="text-white">Transport</span></h1>
+
+        <p className='text-center text-white font-popins text-3xl font-semibold mt-10'>for your Business & Personal Needs</p>
         </div>
     </div>
 </>
